e147c82a60eb65652f6a7091bd1158b77f2e868b
jesusMAes <jesusmarmolesp@gmail.com>
2022-08-21 19:03:42 +0200

    skill section made

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,6 @@
 //here modifies dom of portfolio, not game
 
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 
 const burguerButton = document.getElementById('burguerButton')
@@ -8,11 +9,35 @@ burguerButton.addEventListener('click', (e)=>{
   e.currentTarget.classList.toggle(activeClass)
 })
 
-let tl = gsap.timeline()
+const navButtons = gsap.utils.toArray(".nav-item a");
+const panels =gsap.utils.toArray(".panel");
+console.log(navButtons)
+console.log(panels)
 
+panels.forEach((panel,i) =>{
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "start 50%",
+    onEnter:()=>{
+      navButtons.forEach((e)=>{
+        e.classList.remove("active");
+      });
+      navButtons[i].classList.add("active");
+    },
+    onEnterBack: ()=>{
+      navButtons.forEach((e) =>{
+        e.classList.remove("active");
+      });
+      navButtons[i].classList.add("active");
+    }
+  });
+});
+
+let tl = gsap.timeline()
+//intro effect
 tl.from('#myname',{
   x:-1000,
-  duration:.2,
+  duration:.5,
 })
 tl.to('#myname',{
   x:150,
